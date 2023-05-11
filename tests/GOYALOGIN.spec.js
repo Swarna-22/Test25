13be251139ec1df5db3d310d7f76d2c5f24642f2
Swarna-22 <priyadarshini.panda@datatusk.com>
2023-05-12 03:21:36 +0530

    Goya login

--- tests/GOYALOGIN.spec.js
+++ tests/GOYALOGIN.spec.js
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test';
-
+//Goya Login
 test('test', async ({ page }) => {
   await page.goto('https://portal-test.goya.com/oms2/#/login');
   await page.getByPlaceholder('Username').click();
-  await page.getByPlaceholder('Username').fill('013507');
+  await page.getByPlaceholder('Username').fill('013506');
   await page.getByPlaceholder('Username').press('Tab');
   await page.getByPlaceholder('Password').fill('Pwd@013506');
   await page.getByRole('button', { name: 'Login' }).click();
